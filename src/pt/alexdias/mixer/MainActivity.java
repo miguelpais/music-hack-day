e9afaac91df4ed733de8b2a13c09ffbf453a01ff
Alexandre Dias <alexnvdias@gmail.com>
2013-01-19 13:35:21 +0100

    Test commit.

--- src/pt/alexdias/mixer/MainActivity.java
+++ src/pt/alexdias/mixer/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity {
         // iterate through files in the folder and set the MediaMetadataRetriever source to each of them
         
         File[] trackFiles = new File("/sdcard/mixer").listFiles();
+        String test = "";
         
         for(File f : trackFiles) {
         	
