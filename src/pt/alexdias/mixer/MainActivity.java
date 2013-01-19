8d206e302f07f12a2d9be681ce9435a03b49a416
Miguel Pais <miguelpais@gmail.com>
2013-01-19 18:05:58 +0100

    Implementes get BPMs functionality

--- src/pt/alexdias/mixer/MainActivity.java
+++ src/pt/alexdias/mixer/MainActivity.java
@@ -1,14 +1,7 @@
 package pt.alexdias.mixer;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.ExecutionException;
-
-import android.media.MediaMetadataRetriever;
+import pt.alexdias.mixer.domain.Track;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -21,53 +14,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-        
         // iterate through files in the folder and set the MediaMetadataRetriever source to each of them
         
         File[] trackFiles = new File("/sdcard/mixer").listFiles();
         
         for(File f : trackFiles) {
-        	
-        	Log.v("TEST", "Track name: "+f.getName());
-        
-        	// get their metadata (artist and title) here
-        	
-        	mmr.setDataSource(f.getAbsolutePath());
-        	
-        	Log.v("TEST", "Track artist: "+ mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
-        	Log.v("TEST", "Track name: "+ mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
-        	
-        	URL echonestSearch;
-        	String jsonString = "";
-        	
-        	
-        	
-        	try {
-				echonestSearch = new URL("http://developer.echonest.com/api/v4/song/search?api_key=" +
-						"M3ATLVAJVEGQGRJWN&format=json&artist=" +
-						mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).replace(" " , "%20") +
-						"&title=" +
-						mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE).replace(" " , "%20")
-						);
-			
-				jsonString = new NetworkAccessTask().execute(echonestSearch).get();
-				
-
-	        Log.v("TEST", "Response: "+ jsonString);
-				
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
-
-        	
+        	Track track = new Track(f.getAbsolutePath());
+        	Log.v("TEST", track.getBPM().toString());
         }
-        
-        
     }
 
     @Override
